7a83c231011e9ae98e9355f43f506df25fbc58dd
Manesh Barukula <MBarukula@careered.com>
2019-10-14 02:57:56 -0500

    made changes

--- FMATS/Employee/AddAgent.aspx
+++ FMATS/Employee/AddAgent.aspx
@@ -19,8 +19,7 @@
                     <div class="panel-body">
 
                         <form class="form-horizontal" id="AddAgent" action="#" method="post">
-
-
+                            <asp:HiddenField runat="server" id="AgentId"/>
                             <div class="row">
 
                                 <div class="col-md-6">
@@ -29,7 +28,6 @@
                                         <input type="text" name="txtname" runat="server" class="form-control" id="txtname" placeholder="Name" data-bv-field="txtname" />
                                         <small class="help-block" data-bv-validator="notEmpty" data-bv-for="txtname" data-bv-result="NOT_VALIDATED" style="display: none;">The Name is required and can't be empty</small>
                                     </div>
-
                                 </div>
 
                                 <div class="col-md-6">
@@ -38,18 +36,13 @@
                                         <label>Agent Type</label>
 
                                         <select class="form-control" id="ddlagenttype" runat="server" name="ddlagenttype" data-bv-field="ddlagenttype">
-                                            <option value="">None</option>
+                                            <option selected="selected" value="">None</option>
                                             <option value="1">Quick Agent</option>
                                             <option value="2">Full Agent</option>
-
                                         </select>
                                         <small class="help-block" data-bv-validator="notEmpty" data-bv-for="ddlagenttype" data-bv-result="NOT_VALIDATED" style="display: none;">Please select atleast one Type</small>
-
                                     </div>
-
                                 </div>
-
-
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label>Agent Employee Name</label>
@@ -58,7 +51,6 @@
                                     </div>
                                     <!-- /.form-group -->
                                 </div>
-
                                 <div class="col-md-6">
                                     <!-- /.form-group -->
                                     <div class="form-group">
@@ -82,9 +74,6 @@
                                     </div>
                                     <!-- /.form-group -->
                                 </div>
-                               
-                               
-
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label>Agent Email</label>
@@ -109,21 +98,64 @@
                                     </div>
                                     <!-- /.form-group -->
                                 </div>
-
-
-
-
-
                             </div>
-
                             <div class="row">
                                 <div class="col-md-9"></div>
                                 <div class="col-md-3">
-                                    <button type="submit" class="btn btn-primary pull-right">Submit</button>
+                                    <button type="button" runat="server" ID="btnSubmit" OnServerClick="btn_addagent_Click" class="btn btn-primary pull-right">Submit</button>
                                 </div>
                             </div>
-
                         </form>
+                    </div>
+                </div>
+                
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Agents</h3>
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse">
+                                <i class="fa fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body" style="display: block;">
+                        <div class="row">
+                            <div class="box-body table-responsive">
+                                <asp:GridView runat="server" id="AgentGrid" CssClass="table table-hover"
+                                              ShowHeaderWhenEmpty="true" 
+                                              AutoGenerateColumns="false" DataKeyNames="AgentId" OnRowEditing="AgentGrid_OnRowEditing"
+                                              OnPageIndexChanging="AgentGrid_PageIndexChanging" OnRowCommand="AgentGrid_OnRowCommand"
+                                              OnRowDataBound="AgentGrid_OnRowDataBound" OnRowDeleting="AgentGrid_OnRowDeleting"
+                                              OnRowCancelingEdit="AgentGrid_RowCancelingEdit" Width="100%">
+                                    <EmptyDataTemplate>No Records to fetch..!</EmptyDataTemplate>
+                                    <Columns>  
+                                        <asp:BoundField DataField="AgentId" HeaderText="#" ReadOnly="True" />  
+                                        <asp:BoundField DataField="AgentName" HeaderText="Name" ReadOnly="True" />
+                                        <asp:BoundField DataField="AgentType" HeaderText="Agent Type" ReadOnly="True" />  
+                                        <asp:BoundField DataField="EmployeeName" HeaderText="Employee" ReadOnly="True" />
+                                        <asp:BoundField DataField="AccessLevels" HeaderText="Access Level" ReadOnly="True" />  
+                                        <asp:BoundField DataField="Mobile" HeaderText="Mobile" ReadOnly="True" />
+                                        <asp:BoundField DataField="Email" HeaderText="Email" ReadOnly="True" />  
+                                        <asp:BoundField DataField="ScanPref" HeaderText="Scan Pref" ReadOnly="True" />
+                                        <asp:BoundField DataField="Section" HeaderText="Section" ReadOnly="True" />  
+                                        <asp:BoundField DataField="AgentMisc1" HeaderText="Agent Misc1" ReadOnly="True" />
+                                        <asp:BoundField DataField="AgentMisc2" HeaderText="Agent Misc2" ReadOnly="True" />  
+                                        <asp:BoundField DataField="AgentMisc3" HeaderText="Agent Misc3" ReadOnly="True" />
+                                        <asp:BoundField DataField="CreatedDate" DataFormatString="{0:d}" HeaderText="Created" ReadOnly="True" />
+                                        <asp:TemplateField>
+                                            <ItemTemplate>
+                                                <asp:ImageButton ImageUrl="~/img/icons8-pencil-100.png" CommandArgument='<%#Eval("AgentId") %>'
+                                                                 ID="btnEdit" CommandName="Edit" runat="server" ToolTip="Edit"
+                                                            Height="20px" Width="20px" />
+                                                <asp:ImageButton ID="btnDelete" CommandName="Delete" ImageUrl="~/img/icons8-trash-20.png" runat="server"
+                                                            ToolTip="Delete" Height="20px" Width="20px" CommandArgument='<%#Eval("AgentId") %>' />
+                                            </ItemTemplate>
+                                        </asp:TemplateField>
+                                    </Columns>
+                                </asp:GridView>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
--- FMATS/Employee/AddAgent.aspx.cs
+++ FMATS/Employee/AddAgent.aspx.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FMATS.DAL;
 
 namespace FMATS.Employee
 {
@@ -11,12 +13,207 @@ namespace FMATS.Employee
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                //if (!this.Page.User.Identity.IsAuthenticated)
+                //{
+                //    FormsAuthentication.RedirectToLoginPage();
+                //}
+                //else
+                {
+                    BindAgentsData();
+                }
+            }
+        }
+
+        private void BindAgentsData()
+        {
+            try
+            {
+                using (FileTrackingContainer container = new FileTrackingContainer())
+                {
+                    var data = (from query in container.Agents
+                                join al in container.AgentAccessLevels on query.AgentAccessLevelId equals al.AgentAccessLevelId
+                                join at in container.AgentTypes on query.AgentTypeID equals at.AgentTypeId
+                                select new
+                                {
+                                    query.AgentName,
+                                    query.AgentId,
+                                    AgentType = at.Description,
+                                    EmployeeName = query.AgentEmployeeName,
+                                    AccessLevels = al.AccessLevel,
+                                    Mobile = query.AgentMobile,
+                                    Email = query.AgentEmail,
+                                    ScanPref = query.AgentScanPref,
+                                    Section = query.AgentSection,
+                                    query.AgentMisc1,
+                                    query.AgentMisc2,
+                                    query.AgentMisc3,
+                                    query.CreatedDate
+                                }).ToList();
+                    if (data.Count > 0)
+                    {
+                        AgentGrid.DataSource = data;
+                        AgentGrid.DataBind();
+                    }
+                    else
+                    {
+                        AgentGrid.DataSource = new List<DAL.Agent>();
+                        AgentGrid.DataBind();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
 
         }
 
         protected void btn_addagent_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (btnSubmit.InnerText == "Submit")
+                {
+                    using (FileTrackingContainer container = new FileTrackingContainer())
+                    {
+                        container.Agents.Add(new DAL.Agent
+                        {
+                            AgentName = txtname.Value,
+                            AgentTypeID = Convert.ToInt32(ddlagenttype.Items[ddlagenttype.SelectedIndex].Value),
+                            AgentEmployeeName = txtAgentEmployeeName.Value,
+                            AgentAccessLevelId = Convert.ToInt32(ddlagentaccess.Items[ddlagentaccess.SelectedIndex].Value),
+                            AgentMobile = Convert.ToInt64(txtagentmobile.Value),
+                            AgentEmail = txtemail.Value,
+                            AgentScanPref = txtagentscanpref.Value,
+                            AgentSection = txtagentsec.Value,
+                            CreatedBy = 1,
+                            CreatedDate = DateTime.Now
+                        }
+                        );
+                        container.SaveChanges();
+                    }
+                }
+                else if (btnSubmit.InnerText == "Update")
+                {
+                    using (FileTrackingContainer container = new FileTrackingContainer())
+                    {
+                        var id = int.Parse(AgentId.Value);
+                        var resultToUpdate =
+                            container.Agents.SingleOrDefault(item => item.AgentId == id);
 
+                        if (resultToUpdate != null)
+                        {
+                            resultToUpdate.AgentName = txtname.Value;
+                            resultToUpdate.AgentTypeID =
+                                Convert.ToInt32(ddlagenttype.Items[ddlagenttype.SelectedIndex].Value);
+                            resultToUpdate.AgentEmployeeName = txtAgentEmployeeName.Value;
+                            resultToUpdate.AgentAccessLevelId =
+                                Convert.ToInt32(ddlagentaccess.Items[ddlagentaccess.SelectedIndex].Value);
+                            resultToUpdate.AgentMobile = Convert.ToInt64(txtagentmobile.Value);
+                            resultToUpdate.AgentEmail = txtemail.Value;
+                            resultToUpdate.AgentScanPref = txtagentscanpref.Value;
+                            resultToUpdate.AgentSection = txtagentsec.Value;
+                            resultToUpdate.ModifiedBy = 1;
+                            resultToUpdate.ModifiedDate = DateTime.Now;
+
+                        }
+
+                        container.SaveChanges();
+                    }
+                }
+
+                //clear values
+                txtname.Value = string.Empty;
+                ddlagenttype.SelectedIndex = 0;
+                txtAgentEmployeeName.Value = string.Empty;
+                ddlagentaccess.SelectedIndex = 0;
+                txtagentmobile.Value = string.Empty;
+                txtemail.Value = string.Empty;
+                txtagentscanpref.Value = string.Empty;
+                txtagentsec.Value = string.Empty;
+                BindAgentsData();
+            }
+            catch (Exception exception)
+            {
+
+            }
+        }
+        protected void AgentGrid_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+
+        }
+
+        protected void AgentGrid_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+
+        }
+
+        protected void AgentGrid_OnRowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int rowIndex = Convert.ToInt32(e.CommandArgument);
+            if (e.CommandName == "Edit")
+            {
+                AgentId.Value = e.CommandArgument.ToString();
+                txtname.Value = AgentGrid.Rows[rowIndex - 1].Cells[1].Text;
+                var agentType = AgentGrid.Rows[rowIndex - 1].Cells[2].Text;
+                ddlagenttype.Items[0].Selected = false;
+                ddlagenttype.Items.FindByText(agentType).Selected = true;
+                txtAgentEmployeeName.Value = AgentGrid.Rows[rowIndex - 1].Cells[3].Text;
+                var access = AgentGrid.Rows[rowIndex - 1].Cells[4].Text;
+                ddlagentaccess.Items[0].Selected = false;
+                ddlagentaccess.Items.FindByText(access).Selected = true;
+                txtagentmobile.Value = AgentGrid.Rows[rowIndex - 1].Cells[5].Text;
+                txtemail.Value = AgentGrid.Rows[rowIndex - 1].Cells[6].Text;
+                txtagentscanpref.Value = AgentGrid.Rows[rowIndex - 1].Cells[7].Text;
+                txtagentsec.Value = AgentGrid.Rows[rowIndex - 1].Cells[8].Text;
+
+                btnSubmit.InnerText = "Update";
+            }
+            else if (e.CommandName == "Delete")
+            {
+                using (FileTrackingContainer container = new FileTrackingContainer())
+                {
+                    var agentToRemove = container.Agents.Find(rowIndex);
+                    container.Agents.Remove(agentToRemove);
+                    container.SaveChanges();
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), Guid.NewGuid().ToString(), "toastr.success('Deleted sucessfully');", true);
+                    BindAgentsData();
+                }
+            }
+        }
+
+        protected void AgentGrid_OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            
+        }
+
+        protected void AgentGrid_OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                // loop all data rows
+                foreach (DataControlFieldCell cell in e.Row.Cells)
+                {
+                    // check all cells in one row
+                    foreach (Control control in cell.Controls)
+                    {
+                        // Must use LinkButton here instead of ImageButton
+                        // if you are having Links (not images) as the command button.
+                        ImageButton button = control as ImageButton;
+                        if (button != null && button.CommandName == "Delete")
+                            // Add delete confirmation
+                            button.OnClientClick = "if (!confirm('Are you sure you want to delete Agent Id = " +
+                                                   e.Row.Cells[0].Text + "?')) return;";
+                    }
+                }
+            }
+        }
+
+        protected void AgentGrid_OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            
         }
     }
 }
